31a1d8fa3b63e705048d820ca4255e66c7da9818
suhrobcoder <suhrobcoder@gmail.com>
2021-06-13 19:57:29 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# movie_info_app_flutter
+
+Flutter app for displaying movies using the themoviedb api.
+
+## Screenshots
+
+Coming soon...
---
+++ android/app/src/main/kotlin/com/example/movie_info_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movie_info_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
