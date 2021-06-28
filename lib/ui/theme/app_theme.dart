2bae5b737b800e2e2ffc5728de7da0ebee9d2eac
suhrobcoder <suhrobcoder@gmail.com>
2021-06-28 22:21:10 +0500

    app theme setup

---
+++ lib/ui/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+var appTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: purple500,
+  accentColor: teal200,
+  fontFamily: "SFPro",
+);
---
+++ lib/ui/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color purple200 = Color(0xFFBB86FC);
+Color purple500 = Color(0xFF6200EE);
+Color purple700 = Color(0xFF3700B3);
+Color teal200 = Color(0xFF03DAC5);
