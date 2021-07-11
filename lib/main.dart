790534761538f903e928c60019e9f60ece18e8d7
suhrobcoder <suhrobcoder@gmail.com>
2021-07-11 14:48:52 +0500

    splash screen setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:movie_info_app_flutter/service_locator.dart';
 import 'package:movie_info_app_flutter/ui/screens/home/home_screen.dart';
 import 'package:movie_info_app_flutter/ui/theme/app_theme.dart';
+import 'package:movie_info_app_flutter/ui/theme/colors.dart';
 
 void main() {
   setup();
@@ -14,8 +15,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarIconBrightness: Brightness.light),
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: backgroundColor,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
     );
     return MaterialApp(
       title: 'Movie Info App',
